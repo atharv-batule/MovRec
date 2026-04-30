39f499efe08de4c751502cf566bea9f4ac4222cf
Atharv Batule <atharvbatule9@gmail.com>
2026-04-30 21:21:25 +0530

    add db

---
+++ backend/db/db.py
@@ -0,0 +1,14 @@
+# db.py
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+key_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate(key_path)
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
